e9db7aa5b21c663edb690ad4e80743a504e933fa
Takács Péter <nvidiaize@gmail.com>
2025-08-29 22:42:50 +0200

    videok es emgucv

--- Face_blur/Face_blur/Program.cs
+++ Face_blur/Face_blur/Program.cs
@@ -1,3 +1,16 @@
 ﻿using System.IO;
+using System;
+using Emgu.CV;
 
-Console.WriteLine("Hello, World!");
+
+namespace Face_blur
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var video = File.Open("/home/taki04/Programozás/Face_blur/testvideos/test1.mp4", FileMode.Open);
+            Console.WriteLine(CvInvoke.BuildInformation);
+        }
+    }
+}
--- Face_blur/Face_blur/obj/Debug/net9.0/Face_blur.AssemblyInfo.cs
+++ Face_blur/Face_blur/obj/Debug/net9.0/Face_blur.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Face_blur")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5afc02aa6cd9a5ff7b858f9571f275d6fcb0df9e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+979912df348dda082d07e71ae64f1e13a305d1d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Face_blur")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Face_blur")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
